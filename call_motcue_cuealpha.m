473e750451d3c924147d398c3632eb289f038d87
zumerj <zumerj@bham.ac.uk>
2019-07-30 17:19:04 +0100

    changed motcue_cuealpha_vs_behav name

--- call_motcue_cuealpha.m
+++ call_motcue_cuealpha.m
@@ -1,6 +1,13 @@
 % call_motcue_cuealpha
 
-subuse=[3:23];
+% subuse=[3:23 2];
+% subuse=[4:23 2];
+% subuse=[3:21 23 2];
+% subuse=[16:23 2];
+subuse=[22:23 2];
 for ii=subuse
-  motcue_cuealpha(ii)
+  motcue_cuealpha_vs_behav(ii)
+%   motcue_cuealpha(ii)
 end
+
+
