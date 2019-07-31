12430e444d33ed708b7dca1a4b33a4c010f45ec8
zumerj <zumerj@bham.ac.uk>
2019-07-31 16:41:24 +0100

    added jump artifact

--- motcue_artifact_all.m
+++ motcue_artifact_all.m
@@ -1,6 +1,11 @@
 function data_out=motcue_artifact_all(data,ii,ff,sub,adir,visflag)
 % function data_out=motcue_artifact_all(data,ii,ff,sub,adir,visflag)
 
+cfg=[];
+cfg.channel={'MEG'}; % get rid of eye ADC channels
+data=ft_selectdata(cfg,data);
+
+%% JUMP
 % replace with Nan then interpolate, where there is a 'jump' (it's usually only 1-2 samples long)
 artfct_jumpz=load([adir sub{ii} '_jump_artfct_runff' num2str(ff) '.mat'],'artfct_jumpz');
 cfg=[];
@@ -16,6 +21,44 @@ for tt=1:length(megeye_jumprm_nan.trial)
   [bricksizes] = arrayfun(@(x) numel(find(bricks==x)), [1:bricknumber] );
   minibricks = (find(bricksizes <= 0.01*megeye_jumprm_nan.fsample));%% find bricks of data that are too small
   megeye_jumprm_nan.trial{tt}(:,ismember(bricks, minibricks))=nan;
+end
+if ii==3 && ff==2 % trial 64 has large artifact
+  cfg=[];
+  cfg.trials=[1:63 65:numel(megeye_jumprm_nan.trial)];
+  megeye_jumprm_nan=ft_selectdata(cfg,megeye_jumprm_nan);
+end
+if ii==18 && ff==4 % trial 64 has large artifact
+  cfg=[];
+  cfg.trials=[1:106 108:numel(megeye_jumprm_nan.trial)];
+  megeye_jumprm_nan=ft_selectdata(cfg,megeye_jumprm_nan);
+end
+if ii==3 && ff==3 % trial 116 goes to edge; can't solve by setting postwindow to 0
+  megeye_jumprm_nan.trial{116}(:,end-2:end)=repmat(megeye_jumprm_nan.trial{116}(:,end-3),[1 3]);
+end
+if ii==3 && ff==4 % trial 116 goes to edge; can't solve by setting postwindow to 0
+  megeye_jumprm_nan.trial{116}(:,end-5:end)=repmat(megeye_jumprm_nan.trial{116}(:,end-6),[1 6]);
+end
+if ii==6 && ff==4 % trial X goes to edge; can't solve by setting postwindow to 0
+  megeye_jumprm_nan.trial{34}(:,end-8:end)=repmat(megeye_jumprm_nan.trial{34}(:,end-9),[1 9]);
+end
+if ii==6 && ff==5 % trial X goes to edge; can't solve by setting postwindow to 0
+  megeye_jumprm_nan.trial{7}(:,end-6:end)=repmat(megeye_jumprm_nan.trial{7}(:,end-7),[1 7]);
+end
+if ii==12 && ff==3 % trial X goes to edge; can't solve by setting postwindow to 0
+  megeye_jumprm_nan.trial{68}(:,end-16:end)=repmat(megeye_jumprm_nan.trial{68}(:,end-17),[1 17]);
+end
+if ii==16 && ff==3 % trial X goes to edge; can't solve by setting postwindow to 0
+  megeye_jumprm_nan.trial{35}(:,end-3:end)=repmat(megeye_jumprm_nan.trial{35}(:,end-4),[1 4]);
+end
+if ii==16 && ff==6 % trial X goes to edge; can't solve by setting postwindow to 0
+  megeye_jumprm_nan.trial{85}(:,end-12:end)=repmat(megeye_jumprm_nan.trial{85}(:,end-13),[1 13]);
+end
+if ii==20 && ff==2 % trial X goes to edge; can't solve by setting postwindow to 0
+  megeye_jumprm_nan.trial{40}(:,end-21:end)=repmat(megeye_jumprm_nan.trial{40}(:,end-22),[1 22]);
+  megeye_jumprm_nan.trial{53}(:,end-9:end)=repmat(megeye_jumprm_nan.trial{53}(:,end-10),[1 10]);
+end
+if ii==20 && ff==5 % trial X goes to edge; can't solve by setting postwindow to 0
+  megeye_jumprm_nan.trial{15}(:,end-17:end)=repmat(megeye_jumprm_nan.trial{15}(:,end-18),[1 18]);
 end
 cfg = [];
 cfg.method = 'pchip'; % Here you can specify any method that is supported by interp1: 'nearest','linear','spline','pchip','cubic','v5cubic'
@@ -53,7 +96,7 @@ if visflag
   cfg.method='summary';
   cfg.keeptrial='yes';
   cfg.keepchannel='yes';
-  %   cfg.channel={'MEG'};
+%   cfg.channel={'MEG'};
   megeye_userej=ft_rejectvisual(cfg,data_out);
   %     cfg=[];
   %     cfg.method='channel';
@@ -81,15 +124,17 @@ elseif ~visflag
     cfg.artfctdef.summary.artifact=artfct_vissummary.artfct_summary.artifact;
     data_out = ft_rejectartifact(cfg, data_out);
   end
-  if ~isempty(setdiff(data_out.label,artfct_vissummary.chankeep))
-    missingchannel=setdiff(data_out.label,artfct_vissummary.chankeep);
+  adcchan={'UADC001' 'UADC002' 'UADC003'};
+  if ~isempty(setdiff(setdiff(data_out.label,artfct_vissummary.chankeep),adcchan))
+    missingchannel=setdiff(setdiff(data_out.label,artfct_vissummary.chankeep),adcchan);
     cfg=[];
     cfg.channel=artfct_vissummary.chankeep;
     data_out=ft_selectdata(cfg,data_out);
     load ctf275_neighb.mat
     cfg=[];
     cfg.neighbours=neighbours;
-    cfg.senstype='meg';
+    cfg.senstype={'MEG'};
+    cfg.grad=data_out.grad;
     cfg.missingchannel=missingchannel;
     data_out=ft_channelrepair(cfg,data_out);
   end
